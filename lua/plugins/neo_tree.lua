94ba0cab660edcd9af4b81b729a86261ff976b20
WinterEffect <yishaigame999@gmail.com>
2024-04-30 10:51:23 +0000

    new feature - see hidden files in neotree

--- lua/plugins/neo_tree.lua
+++ lua/plugins/neo_tree.lua
@@ -1,5 +1,17 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+        filesystem = {
+             filtered_items = {
+             visible = true,
+             show_hidden_count = true,
+             hide_dotfiles = false,
+             hide_gitignored = true,
+             hide_by_name = {},
+             never_show = {'.git'},
+            }
+        }
+    },
     branch = "v3.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
